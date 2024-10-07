5d9da9f9e63e1cbf72f2e17f36529413a9b8cc6c
Thiago <thiagosilva1@unipam.edu.br>
2024-10-07 18:31:46 -0300

    feat(retorno-endpoint): deixando o retorno em portugues

--- src/weather/interfaces/weather-response.interface.ts
+++ src/weather/interfaces/weather-response.interface.ts
@@ -1,5 +1,4 @@
 export interface WeatherInfo {
-  main: string;
   description: string;
 }
 
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -12,7 +12,7 @@ export class WeatherService {
   constructor(private prisma: PrismaService) {}
   async getWeatherInfos() {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=-18.579250&lon=-46.518589&appid=${process.env.API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=-18.579250&lon=-46.518589&appid=${process.env.API_KEY}&units=metric&lang=pt_br`
     );
 
     const weatherResponse = response.data;
@@ -26,7 +26,6 @@ export class WeatherService {
       temp: tempInfos.temp,
       feels_like: tempInfos.feels_like,
       main: weatherInfos.description,
-      description: weatherInfos.main,
     };
   }
   calculateEfficiency(temperature): number {
@@ -37,10 +36,10 @@ export class WeatherService {
     return efficiency;
   }
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async efficiencySyncDB() {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=-18.579250&lon=-46.518589&appid=${process.env.API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=-18.579250&lon=-46.518589&appid=${process.env.API_KEY}&units=metric&lang=pt_br`
     );
 
     const currentTemperature = response.data.main.temp;
